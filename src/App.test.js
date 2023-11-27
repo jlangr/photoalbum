b0a21ceb15c8a2ce3a686ab4af1a9e3881714ea4
Jeff L <jeff@langrsoft.com>
2023-11-27 09:21:10 -0700

    first failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('renders learn react page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  it('contains photo album component', () => {
+    expect(screen.getByText('Photo Album')).toBeInTheDocument();
+  })
 });
