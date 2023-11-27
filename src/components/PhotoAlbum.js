d03f96dec029e093c75a6787fbefd8f44d4454b4
Jeff L <jeff@langrsoft.com>
2023-11-27 09:56:39 -0700

    add and iterate photos on click

--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,9 +1,22 @@
+import {useState} from "react";
+
 export const PhotoAlbum = () => {
+  const [photos, setPhotos] = useState([])
+
+  const retrievePhotos = () => {
+    setPhotos([...photos, {name: 'photo 1'}])
+  }
+
   return (
     <>
       <header className='App-header'>Photo Album</header>
-      <button className='App-button'>
+      <button onClick={retrievePhotos} className='App-button'>
         Retrieve photos
       </button>
+      <div id='photos'>
+        {photos.map(photo => (
+          <p role='article'>photo {photo.name}</p>
+        ))}
+      </div>
     </>)
 }
--- src/components/__test__/PhotoAlbum.test.js
+++ src/components/__test__/PhotoAlbum.test.js
@@ -1,5 +1,5 @@
 import {PhotoAlbum} from '../PhotoAlbum'
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 
 describe('PhotoAlbum', () => {
   it('renders with button to retrieve photos', () => {
@@ -9,5 +9,19 @@ describe('PhotoAlbum', () => {
 
     expect(button).toBeInTheDocument()
     expect(button).toHaveTextContent('Retrieve photos')
+  })
+
+  it('does not have any photos on render', () => {
+    render(<PhotoAlbum/>)
+    expect(screen.queryByRole('article')).not.toBeInTheDocument()
+  })
+
+  it('retrieves photos on button click', () => {
+    render(<PhotoAlbum/>)
+    const button = screen.getByRole('button')
+
+    fireEvent.click(button)
+
+    expect(screen.getByRole('article')).toBeInTheDocument()
   })
 })
